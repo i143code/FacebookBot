d72969fe2a018c6922e59a50b2df6b3e00b4c0f5
i143code <ranjanah143code@yahoo.com>
2016-04-30 10:55:04 -0400

    hello world

--- index.js
+++ index.js
@@ -87,7 +87,7 @@ function sendGenericCrime(sender) {
 				"elements": [{
 					"title": "Murder",
 					"subtitle": "Element #1 of an hscroll",
-					"image_url": "http://messengerdemo.parseapp.com/img/murder.png",
+					"image_url": "http://gdurl.com/zJpr",
 					"buttons": [{
 						"type": "web_url",
 						"url": "https://www.messenger.com",
